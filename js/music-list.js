76d64fb2b6cb7044ab941e661eeaf9960a7c9fd7
Divya <107193443+divyaisdivya@users.noreply.github.com>
2023-12-26 17:48:04 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+  {
+    name: "Paper Rings - Fantasy",
+    artist: "Taylor Swift",
+    img: "music-1",
+    src: "music-1"
+  },
+  { 
+    name: "Makes you beautiful – Flower",
+    artist: "One Direction",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "I'll be waiting - Love",
+    artist: "Cian Ducrot",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Stamding By You - Want Me",
+    artist: "Nish",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Lover - Sun Goes Down",
+    artist: "Taylor Swift",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Dimple - Angel",
+    artist: "BTS",
+    img: "music-6",
+    src: "music-6"
+  },
+
+];
